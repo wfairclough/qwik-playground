c3cd638f0b9176f1994a03bf23818e1976b71003
Will Fairclough <wfairclough@gmail.com>
2023-03-07 22:59:15 -0500

    Auto layout the workflow

--- packages/qwik-flow/src/integrations/react/flow.tsx
+++ packages/qwik-flow/src/integrations/react/flow.tsx
@@ -10,16 +10,59 @@ import ReactFlow, {
   useNodesState,
   useEdgesState,
 } from "reactflow";
+import dagre from "dagre";
 
-import { nodes as initialNodes, edges as initialEdges } from "./init-elements";
+const dagreGraph = new dagre.graphlib.Graph();
+dagreGraph.setDefaultEdgeLabel(() => ({}));
+
+const nodeWidth = 172;
+const nodeHeight = 36;
+
+const nodeTypes = {
+  "scope-approval": DragHandleNode,
+};
+
+const getLayoutedElements = (nodes, edges, direction = "TB") => {
+  const isHorizontal = direction === "LR";
+  dagreGraph.setGraph({ rankdir: direction });
+
+  nodes.forEach((node) => {
+    dagreGraph.setNode(node.id, { width: nodeWidth, height: nodeHeight });
+  });
+
+  edges.forEach((edge) => {
+    dagreGraph.setEdge(edge.source, edge.target);
+  });
+
+  dagre.layout(dagreGraph);
+
+  nodes.forEach((node) => {
+    const nodeWithPosition = dagreGraph.node(node.id);
+    // node.targetPosition = isHorizontal ? 'left' : 'top';
+    // node.sourcePosition = isHorizontal ? 'right' : 'bottom';
+
+    // We are shifting the dagre node position (anchor=center center) to the top left
+    // so it matches the React Flow node anchor point (top left).
+    node.position = {
+      x: nodeWithPosition.x - nodeWidth / 2,
+      y: nodeWithPosition.y - nodeHeight / 2,
+    };
+
+    return node;
+  });
+
+  return { nodes, edges };
+};
+
+// import { nodes as initialNodes, edges as initialEdges } from "./init-elements";
+import { workflow } from "./workflow";
+import DragHandleNode from "./DragHandleNode";
 // import CustomNode from './CustomNode';
+
+const { nodes: initialNodes, edges: initialEdges } = workflow;
 
 // import 'reactflow/dist/style.css?inline';
 // import styles from './overview.css?inline';
-
-// const nodeTypes = {
-//   custom: CustomNode,
-// };
 
 const minimapStyle = {
   height: 120,
@@ -28,9 +71,14 @@ const minimapStyle = {
 const onInit = (reactFlowInstance) =>
   console.log("flow loaded:", reactFlowInstance);
 
+const { nodes: layoutedNodes, edges: layoutedEdges } = getLayoutedElements(
+  initialNodes,
+  initialEdges
+);
+
 const OverviewFlow = () => {
-  const [nodes, setNodes, onNodesChange] = useNodesState(initialNodes);
-  const [edges, setEdges, onEdgesChange] = useEdgesState(initialEdges);
+  const [nodes, setNodes, onNodesChange] = useNodesState(layoutedNodes);
+  const [edges, setEdges, onEdgesChange] = useEdgesState(layoutedEdges);
   const onConnect = useCallback(
     (params) => setEdges((eds) => addEdge(params, eds)),
     []
